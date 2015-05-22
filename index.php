f05ac2d4cbe1106dc63c397ed4247871a9160ad0
pederU <pederU@users.noreply.github.com>
2015-05-22 10:48:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 	</script>
 	
 	<!-- Fa-strap -->
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 
 </head>
 <body>

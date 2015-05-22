668b3e753f66f5a0a8c1f1a64ad538edf1752537
pederU <pederU@users.noreply.github.com>
2015-05-22 10:41:04 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -36,8 +36,8 @@ ini_set('include_path', '/volume1/web/fuTelldus/public');
 
 	<!-- Jquery -->
 	<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-	<script src="http://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
-	<link href="http://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
+	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+	<link href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet">
 	<script src="../lib/packages/timeago_jquery/jquery.timeago.js"></script>
 	<?php
 		if ($defaultLang == "sw") echo "<script src=\"../lib/packages/timeago_jquery/jquery.timeago.sw.js\"></script>";

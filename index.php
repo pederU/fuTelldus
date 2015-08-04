00744ae8023debb6249e42b60f2a804ab60b40ce
pederU <pederU@users.noreply.github.com>
2015-08-04 11:30:26 +0200

    Update index.php
    Uses font-awsome 4.4.0

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 	</script>
 	
 	<!-- Fa-strap -->
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 
 </head>
 <body>
